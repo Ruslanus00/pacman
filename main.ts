5e18da84e0202f9c2493095e702c972c91046ae0
Ruslanus00 <90270667+Ruslanus00@users.noreply.github.com>
2021-10-02 21:07:15 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let Pressure_Sensor = 0
+let Potentiometer = 0
+basic.forever(function () {
+    Pressure_Sensor = pins.digitalReadPin(DigitalPin.P0)
+    Potentiometer = pins.analogReadPin(AnalogPin.P1)
+    if (Pressure_Sensor == 1) {
+        images.createBigImage(`
+            . # # # . . . . . .
+            # # # # # . . . . .
+            # # # # # . . . . .
+            # # # # # . . . . .
+            . # # # . . . . . .
+            `).scrollImage(5, 200)
+    } else if (Potentiometer < 300) {
+        images.createBigImage(`
+            . # # # . . . . . .
+            # # # # # . . . . .
+            . . . # # . . . . .
+            # # # # # . . . . .
+            . # # # . . . . . .
+            `).scrollImage(1, 200)
+    } else if (Potentiometer > 900) {
+        images.createBigImage(`
+            . # # # . . . . . .
+            # . # . # . . . . .
+            # # # # # . . . . .
+            # # # # # . . . . .
+            # . # . # . . . . .
+            `).scrollImage(1, 200)
+    }
+})
